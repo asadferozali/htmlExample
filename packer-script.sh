f88d77ea5c1495de1cdd6ac494e945df22e82278
Asad Feroz Ali <asadferozali@gmail.com>
2019-05-27 15:58:49 +0500

    Update packer-script.sh

--- packer-script.sh
+++ packer-script.sh
@@ -2,7 +2,7 @@
 
 sudo yum install httpd -y
 sudo service httpd start
-aws s3 cp s3://loadbalncedemo/{{user `filename`}}.zip {{user `filename`}}.zip
+aws s3 cp s3://{{user `bucketname`}}/{{user `bucket_keyname`}}/{{user `filename`}}.zip {{user `filename`}}.zip
 unzip {{user `filename`}}.zip
 rm {{user `filename`}}.zip
 sudo cp index.html /var/www/html/
